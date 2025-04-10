b646689a9e31c4e7233f3754b6f9701ce1950d9a
joonsol <71648638+joonsol@users.noreply.github.com>
2025-04-10 10:22:04 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/emotion-diary-test/', // 필수: GitHub repo 이름
+  build: {
+    outDir: 'docs', // ← build 결과를 docs 폴더로!
+  },
+})
